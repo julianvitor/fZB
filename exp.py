8889f656452a340bf7a7385bc6b6af04ac67f9bd
julian vitor <julian.vtr@gmail.com>
2018-11-02 15:51:51 -0300

    Diminuindo a quantidade de quadros por segundo e unindo as funções de reconhecimento com criação de quadros

---
+++ exp.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import os
+
+
+
+vidcap = cv2.VideoCapture('video3.mp4')
+count = 0
+success = True
+try:
+    if not os.path.exists('data'):
+        os.makedirs('data')
+except OSError:
+    print('Error: Creating directory of data')
+
+
+
+
+while success:
+  vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*200))
+  success,image = vidcap.read()
+
+  ## Stop when last frame is identified
+  image_last = cv2.imread("frame{}.png".format(count-1))
+  if np.array_equal(image,image_last):
+      break
+
+  cv2.imwrite("./data/frame%d.png" % count, image)     # save frame as PNG file
+  print (count)
+  count += 1
--- fzb.py
+++ fzb.py
@@ -1,32 +1,32 @@
 import cv2
 import numpy as np
 import os
-from PIL import Image # Importando o módulo Pillow para abrir a imagem no script
-import pytesseract # Módulo para a utilização da tecnologia OCR
+from PIL import Image  # Importando o módulo Pillow para abrir a imagem no script
+import pytesseract  # Módulo para a utilização da tecnologia OCR
 import math
 
-def vtf ():
-    
-    # Playing video from file:
-    cap = cv2.VideoCapture('video3.mp4')
-   
 
+def vtf():
+    # Local para carregar o video.
+    cap = cv2.VideoCapture('video3.mp4')
+
+
+    #criando diretorio temporario para armazenar frames
     try:
         if not os.path.exists('data'):
             os.makedirs('data')
     except OSError:
-        print ('Error: Creating directory of data')
+        print('Error: Creating directory of data')
     ret = True
     currentFrame = 0
 
-
-    while(ret):
+    while (ret):
         # Capture frame-by-frame
         ret, frame = cap.read()
 
         # Saves image of the current frame in png file
         name = './data/frame' + str(currentFrame) + '.png'
-        print ('Creating...' + name)
+        print('Creating...' + name)
         cv2.imwrite(name, frame)
 
         # To stop duplicate images
@@ -36,41 +36,28 @@ def vtf ():
     cap.release()
     cv2.destroyAllWindows()
 
-def rec ():
 
+def rec():
+    alpha = float(3)  # Simple contrast control 1-3
+    beta = int(100)  # Simple brightness control 0 -100
 
-    alpha = float(3)     # Simple contrast control 1-3
-    beta = int(100)             # Simple brightness control 0 -100
-     
+    img = cv2.imread('frame566.png')
 
-    img = cv2.imread('imagem4.jpg')
-     
-    mul_img = cv2.multiply(img, np.array([alpha]))                    # mul_img = img*alpha
-    new_img = cv2.add(mul_img, beta)                                  # new_img = img*alpha + beta
+    mul_img = cv2.multiply(img, np.array([alpha]))  # mul_img = img*alpha
+    new_img = cv2.add(mul_img, beta)  # new_img = img*alpha + beta
     cv2.imwrite('contraste.png', new_img)
 
-
-
-    
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #convert color for pil
-    img_pil = Image.fromarray(img) #convert fo pil
-    
-
-
-
-
-    
-
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # convert color for pil
+    img_pil = Image.fromarray(img)  # convert fo pil
 
     img = Image.open('contraste.png')
 
     # convertendo em um array editável de numpy[x, y, CANALS]
-    npimg = np.asarray(img).astype(np.uint8) 
-
+    npimg = np.asarray(img).astype(np.uint8)
 
     # diminuição dos ruidos antes da binarização
-    npimg[:, :, 0] = 0 # zerando o canal R (RED)
-    npimg[:, :, 2] = 0 # zerando o canal B (BLUE)
+    npimg[:, :, 0] = 0  # zerando o canal R (RED)
+    npimg[:, :, 2] = 0  # zerando o canal B (BLUE)
 
     # atribuição em escala de cinza
     im = cv2.cvtColor(npimg, cv2.COLOR_RGB2GRAY)
@@ -80,50 +67,22 @@ def rec ():
     # pixels de intensidade de cor acima de 127 serão convertidos para 255 (BRANCO)
     # A atrubição do THRESH_OTSU incrementa uma análise inteligente dos nivels de truncagem
     ret, thresh = cv2.threshold(im, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    
-    th3 = cv2.adaptiveThreshold(thresh,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
+    th3 = cv2.adaptiveThreshold(thresh, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
     # reconvertendo o retorno do threshold em um objeto do tipo PIL.Image
-    binimagem = Image.fromarray(th3) 
+    binimagem = Image.fromarray(th3)
 
     os.remove("contraste.png")
-
-    
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-
-
-
-
-
-
-
-
-    
-    
-    its = pytesseract.image_to_string(binimagem, lang='por')  # Extraindo o texto da imagem
+    its = pytesseract.image_to_string(binimagem)  # Extraindo o texto da imagem
 
     cv2.imwrite('imagem4pp.png', th3)
-    
-    
+
     print(its)
 
 
-
-
-
-
-
-
-#vtf()
-
-rec()
-
-
-
-
-
-
-
+vtf()
